464140774faa1b1f177f221690426df8f2816d4b
Rahul <rnkr99@gmail.com>
2021-06-06 01:25:45 +0530

    browse button function working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## quickShare - Make files transfer easy.
 
+### Easily share files from one space to another using Node.js and MongoDB to store details on database.
+### Will use heroku to deploy app on server using heroku git and upload files there for easy access.
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const dropZone = document.querySelector(".drop-zone");
+const browseBtn = document.querySelector(".browseBtn");
+const fileinput = document.querySelector("#fileInput");
+
+const host = "https://innshare.herokuapp.com/";
+const uploadURL = `${host}api/files`;
+// const uploadURL = `${host}api/files`;
 
 dropZone.addEventListener("dragover", (e)=>{
     e.preventDefault();
@@ -11,8 +17,34 @@ dropZone.addEventListener("dragover", (e)=>{
 
 dropZone.addEventListener("dragleave", ()=>{
     dropZone.classList.remove("dragged")
-})
+});
 dropZone.addEventListener("drop", (e)=>{
     e.preventDefault();
-    dropZone.classList.remove("dragged")
-})
+    dropZone.classList.remove("dragged");
+    const files = e.dataTransfer.files
+    console.log(files);
+    if(files.length){
+        fileInput.files = files;
+        uploadFile();
+    }
+});
+
+browseBtn.addEventListener("click", ()=>{
+    fileInput.click();
+});
+
+const uploadFile = ()=>{
+
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append("myfile", file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = ()=>{
+        console.log(xhr.readyState)
+    }
+
+    xhr.open("POST", uploadURL);
+    xhr.send(formData);
+
+};
